db8bb3367bd97a015463cf2ebc28307bc0790a9d
alonger <alexander.loptev@gmail.com>
2012-07-20 09:52:36 +0400

    Tries to make two models per admin.

--- orders/admin.py
+++ orders/admin.py
@@ -8,13 +8,14 @@ from django.contrib import admin, messages
 from django.contrib.admin import ModelAdmin
 from orders.models import Work, Order, ExtendedPlan, ExtendedOrder,\
     ExtendedFinishedOrder
-from django.contrib.admin.options import csrf_protect_m
-from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.core import urlresolvers
 from orders.forms import OrderForm, WorkForm
 from persons.models import Dispatcher
 from prices.models import Price, PaymentMethod
+from functools import update_wrapper
+from django.conf.urls import patterns, url
+
 
 class OrderWorkInline(admin.TabularInline):
     form = WorkForm
@@ -34,8 +35,12 @@ class OrderAdmin(ModelAdmin):
             'fields': ('sms', 'cut_sms')
         }),
     )
-    @csrf_protect_m
-    @transaction.commit_on_success
+    def __init__(self, model, admin_site):
+        if model == Order:
+            self.list_display = []
+            self.list_filter = []
+        ModelAdmin.__init__(self, model, admin_site)
+    
     def change_view(self, request, object_id, form_url='', extra_context=None):
         return ModelAdmin.change_view(self, request, object_id, form_url=form_url, extra_context={
                                                                                                   'prices': Price.objects.all().values(),
@@ -53,7 +58,6 @@ class OrderAdmin(ModelAdmin):
     def response_add(self, request, obj, post_url_continue='../%s/'):
         messages.add_message(request, messages.WARNING, u'Заказ добавлен! Тщательно проверьте его на наличие ошибок.')
         return HttpResponseRedirect(urlresolvers.reverse('admin:orders_order_change', args=(obj.id,)))
-    @csrf_protect_m
     def changelist_view(self, request, extra_context=None):
         return HttpResponseRedirect(urlresolvers.reverse('admin:orders_extendedorder_changelist'))
 
@@ -65,20 +69,45 @@ class ExtendedOrderAdmin(ModelAdmin):
     date_hierarchy = 'datetime'
     search_fields = ['customer__name', 'branch__name', 'start', 'payment_method__name', 'dispatcher__name']
     list_select_related = True
-    @csrf_protect_m
-    @transaction.commit_on_success
+    
+    def __init__(self, model, admin_site):
+        self.order_admin = OrderAdmin(Order, admin_site)
+        ModelAdmin.__init__(self, model, admin_site)
+
     def change_view(self, request, object_id, form_url='', extra_context=None):\
-        return HttpResponseRedirect(urlresolvers.reverse('admin:orders_order_change', args=(object_id,)))
+        return self.order_admin.change_view(request, object_id, form_url, extra_context)
     
-    @csrf_protect_m
-    @transaction.commit_on_success
     def delete_view(self, request, object_id, extra_context=None):
-        return HttpResponseRedirect(urlresolvers.reverse('admin:orders_order_delete', args=(object_id,)))
+        return self.order_admin.delete_view(request, object_id, extra_context)
     
-    @csrf_protect_m
-    @transaction.commit_on_success
     def add_view(self, request, form_url='', extra_context=None):
-        return HttpResponseRedirect(urlresolvers.reverse('admin:orders_order_add'))
+        return self.order_admin.add_view(request, form_url, extra_context)
+    
+    def get_urls(self):
+        def wrap(view):
+            def wrapper(*args, **kwargs):
+                return self.admin_site.admin_view(view)(*args, **kwargs)
+            return update_wrapper(wrapper, view)
+        info = self.model._meta.app_label, 'order'
+        urlpatterns = patterns('',
+            url(r'^$',
+                wrap(self.changelist_view),
+                name='%s_%s_changelist' % info),
+            url(r'^add/$',
+                wrap(self.add_view),
+                name='%s_%s_add' % info),
+            url(r'^(.+)/history/$',
+                wrap(self.history_view),
+                name='%s_%s_history' % info),
+            url(r'^(.+)/delete/$',
+                wrap(self.delete_view),
+                name='%s_%s_delete' % info),
+            url(r'^(.+)/$',
+                wrap(self.change_view),
+                name='%s_%s_change' % info),
+        )
+        return super(ExtendedOrderAdmin, self).get_urls() + urlpatterns
+
 
 class WorkAdmin(ModelAdmin):
     list_display = ('order', 'executor', 'fee_through', 'quantity', 'total', 'executor_sum', 'executor_balance')
@@ -87,8 +116,7 @@ class WorkAdmin(ModelAdmin):
     list_select_related = True
     #date_hierarchy = 'order__datetime'
 
-admin.site.register(Order, OrderAdmin)
-admin.site.register(ExtendedOrder, ExtendedOrderAdmin)
 admin.site.register(ExtendedPlan, ExtendedOrderAdmin)
 admin.site.register(ExtendedFinishedOrder, ExtendedOrderAdmin)
+admin.site.register(ExtendedOrder, ExtendedOrderAdmin)
 admin.site.register(Work, WorkAdmin)
--- persons/admin.py
+++ persons/admin.py
@@ -12,13 +12,12 @@ from persons.forms import ExecutorForm
 from orders.models import Work
 from django.contrib import messages
 from django.contrib.auth.admin import csrf_protect_m
-from django.http import HttpResponseRedirect
-from django.core import urlresolvers
-from django.db import transaction
 from datetime import datetime, timedelta
 from django.contrib.admin.filters import SimpleListFilter
 from hephaestus import settings
 from django.db.models.aggregates import Min, Max, Sum
+from functools import update_wrapper
+from django.conf.urls import patterns, url
 
 class PersonAdmin(ModelAdmin):
     readonly_fields = ('total_debt', 'appearance_date',)
@@ -26,32 +25,52 @@ class PersonAdmin(ModelAdmin):
     list_filter = ('branch',)
     search_fields = ['name', 'phone', 'branch__name']
     ordering = ('name',)
+        
     
 class CustomerAdmin(PersonAdmin):
     exclude = ('birthday', 'address',)
-
+    
 class ExecutorAdmin(PersonAdmin):
     form = ExecutorForm
     readonly_fields = ('total_debt', 'appearance_date', 'last_contact',)
-    @csrf_protect_m
-#    def changelist_view(self, request, extra_context=None):
-#        return HttpResponseRedirect(urlresolvers.reverse('admin:persons_extendedexecutor_changelist'))
-    
-    @csrf_protect_m
-    @transaction.commit_on_success
-    def change_view(self, request, object_id, form_url='', extra_context=None):
-        if request.method != 'POST':
-            executor = Executor.objects.get(pk=object_id)
-            delay = (datetime.now() - executor.last_contact).seconds / 60
-            if delay < 60:
-                messages.add_message(request, messages.INFO if delay > 15 else messages.ERROR, 'Исполнитель обзвонен %s минут назад' % delay)
-        return PersonAdmin.change_view(self, request, object_id, form_url=form_url, extra_context=extra_context)
-    
+        
 class ExtendedExecutorAdmin(PersonAdmin):
     list_display = ('name', 'category', 'free_datetime', 'current_order', 'current_order_accepted', 'note', 'phone', 'address', 'total_debt',)
     list_filter = ('branch', 'current_order_accepted')
     search_fields = ['name', 'current_order__customer__name', 'note', 'phone', 'address']
+    #list_editable = ('current_order',)
     ordering = ('-current_order_accepted', 'current_order__id', 'category', 'free_datetime')
+
+    def get_urls(self):
+        def wrap(view):
+            def wrapper(*args, **kwargs):
+                return self.admin_site.admin_view(view)(*args, **kwargs)
+            return update_wrapper(wrapper, view)
+        info = self.model._meta.app_label, 'executor'
+        urlpatterns = patterns('',
+            url(r'^$',
+                wrap(self.changelist_view),
+                name='%s_%s_changelist' % info),
+            url(r'^add/$',
+                wrap(self.add_view),
+                name='%s_%s_add' % info),
+            url(r'^(.+)/history/$',
+                wrap(self.history_view),
+                name='%s_%s_history' % info),
+            url(r'^(.+)/delete/$',
+                wrap(self.delete_view),
+                name='%s_%s_delete' % info),
+            url(r'^(.+)/$',
+                wrap(self.change_view),
+                name='%s_%s_change' % info),
+        )
+        return super(ExtendedExecutorAdmin, self).get_urls() + urlpatterns
+
+    
+    def __init__(self, model, admin_site):
+        self.executor_admin = ExecutorAdmin(Executor, admin_site)
+        return super(ExtendedExecutorAdmin, self).__init__(model, admin_site)
+    
     def reject_order(self, request, queryset):
         for executor in queryset:
             Work.objects.filter(order=executor.current_order, executor=executor).delete()
@@ -68,36 +87,26 @@ class ExtendedExecutorAdmin(PersonAdmin):
         messages.add_message(request, messages.WARNING, 'Заказы помечены как завершенные исполнителями!')
     finish_order.short_description = 'Исполнитель завершил заказ'
     actions = [reject_order, accept_order, finish_order]
-    @csrf_protect_m
-    @transaction.commit_on_success
+    
+
     def change_view(self, request, object_id, form_url='', extra_context=None):
-        response = ExecutorAdmin.change_view(ExecutorAdmin(Executor, self.admin_site), request, object_id, form_url, extra_context)
-        if isinstance(response, HttpResponseRedirect):
-            return HttpResponseRedirect(urlresolvers.reverse('admin:persons_extendedexecutor_changelist',))
-        return response
-        #return HttpResponseRedirect(urlresolvers.reverse('admin:persons_executor_change', args=(object_id,)))
+        if request.method != 'POST':
+            executor = Executor.objects.get(pk=object_id)
+            delay = (datetime.now() - executor.last_contact).seconds / 60
+            if delay < 60:
+                messages.add_message(request, messages.INFO if delay > 15 else messages.ERROR, 'Исполнитель обзвонен %s минут назад' % delay)
+        return self.executor_admin.change_view(request, object_id, form_url, extra_context)
     
-    @csrf_protect_m
-    @transaction.commit_on_success
     def delete_view(self, request, object_id, extra_context=None):
-        response = ExecutorAdmin.delete_view(ExecutorAdmin(Executor, self.admin_site), request, object_id, extra_context)
-        if isinstance(response, HttpResponseRedirect):
-            return HttpResponseRedirect(urlresolvers.reverse('admin:persons_extendedexecutor_changelist',))
-        return response
+        return self.executor_admin.delete_view(request, object_id, extra_context)
     
-    @csrf_protect_m
-    @transaction.commit_on_success
     def add_view(self, request, form_url='', extra_context=None):
-        response = ExecutorAdmin.delete_view(ExecutorAdmin(Executor, self.admin_site), request, form_url, extra_context)
-        if isinstance(response, HttpResponseRedirect):
-            return HttpResponseRedirect(urlresolvers.reverse('admin:persons_extendedexecutor_changelist',))
-        return response
+        return self.executor_admin.add_view(request, form_url, extra_context)
     
     
 class HierarchyDateListFilter(SimpleListFilter):
 
     def lookups(self, request, model_admin):
-        #year
         if not request.GET.has_key(self.parameter_name):
             years = Debt.objects.extra(select={'year': "year(date)"}).distinct().values_list('year', flat=True)
             return map(None, years, years)
@@ -196,6 +205,5 @@ admin.site.register(Person, PersonAdmin)
 admin.site.register(Customer, CustomerAdmin)
 admin.site.register(Broker, PersonAdmin)
 admin.site.register(Dispatcher, PersonAdmin)
-admin.site.register(Executor, ExecutorAdmin)
 admin.site.register(ExtendedExecutor, ExtendedExecutorAdmin)
 admin.site.register(Debt, DebtAdmin)
